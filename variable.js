09e12e5ee0e67e1fcd53871f237c111568f33ac2
Lakshman kumar <lakshmankumar2603@gmail.com>
2024-09-12 13:33:28 +0530

    number and boolean data type

---
+++ variable.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = "Welcome! Lakshman";
+// number 
+var myNum;
+// myNum = "123" ---> this is wrong
+myNum = 123;
+//Note - 
+var myId = 23; // it also detect that myId is a number type
+// myId = "hello";  ---> this also gives error
+var isLoggedIn = false;
+console.log(isLoggedIn.valueOf()); // false
---
+++ variable.ts
@@ -0,0 +1,20 @@
+let greetings: string = "Welcome! Lakshman";
+
+// number 
+
+let myNum: number;
+
+// myNum = "123" ---> this is wrong
+myNum = 123;
+
+//Note - 
+let myId = 23; // it also detect that myId is a number type
+// myId = "hello";  ---> this also gives error
+
+// Boolean
+
+let isLoggedIn: boolean = false;
+isLoggedIn.valueOf(); // false
+
+
+export {}
