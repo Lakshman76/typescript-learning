3fd49ab3df3169b088563628404ff8907fe65d99
Lakshman kumar <lakshmankumar2603@gmail.com>
2024-09-15 17:43:13 +0530

    add more function type

--- myFunction.js
+++ myFunction.js
@@ -1,10 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 function addTwo(num) {
+    // return "hello";
     return num + 2;
 }
 // addTwo("hello"); // --> error but if num is not number type (any) then it will works fine
-addTwo(5);
+var myValue = addTwo(5);
 function getUpper(val) {
     return val.toUpperCase();
 }
@@ -17,3 +18,21 @@ var loginUser = function (name, email, isPaid) {
     if (isPaid === void 0) { isPaid = false; }
 };
 loginUser("lakshman", "lakshman.com");
+var getHello = function (str) {
+    return "hello" + str;
+};
+getHello("world");
+var heros = ["thor", "spiderman", "ironman"];
+// const heros = [1, 2, 3];
+// here string is return type
+heros.map(function (hero) {
+    return "hero is ".concat(hero);
+});
+// * If function is not returning anything then we should use void type
+function consoleError(errmsg) {
+    console.log(errmsg);
+}
+// *The never type represents values which are never observed. In a return type, this means that the function throws an exception or terminates execution of the program.
+function handleError(errmsg) {
+    throw new Error(errmsg);
+}
--- myFunction.ts
+++ myFunction.ts
@@ -1,11 +1,12 @@
-function addTwo(num: number) {
+function addTwo(num: number): number {
+  // return "hello";
   return num + 2;
 }
 
 // addTwo("hello"); // --> error but if num is not number type (any) then it will works fine
-addTwo(5);
+let myValue = addTwo(5);
 
-function getUpper(val) {
+function getUpper(val: string) {
   return val.toUpperCase();
 }
 
@@ -20,5 +21,28 @@ signUpUser("lakshman", "lakshman.com", false);
 let loginUser = (name: string, email: string, isPaid: boolean = false) => {};
 
 loginUser("lakshman", "lakshman.com");
+
+const getHello = (str: string): string => {
+  return "hello" + str;
+};
+getHello("world");
+
+const heros = ["thor", "spiderman", "ironman"];
+// const heros = [1, 2, 3];
+
+// here string is return type
+heros.map((hero): string => {
+  return `hero is ${hero}`;
+});
+
+// * If function is not returning anything then we should use void type
+function consoleError(errmsg: string): void {
+  console.log(errmsg);
+}
+
+// *The never type represents values which are never observed. In a return type, this means that the function throws an exception or terminates execution of the program.
+function handleError(errmsg: string): never {
+  throw new Error(errmsg);
+}
 
 export {};
