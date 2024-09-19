a35ed8dd393972548e71e2cec1e11339396567b9
Lakshman kumar <lakshmankumar2603@gmail.com>
2024-09-19 11:56:07 +0530

    add getter and setter

--- class.js
+++ class.js
@@ -9,7 +9,7 @@ var User1 = /** @class */ (function () {
     return User1;
 }());
 var user1 = new User1("Lakshman", "laksh@google.com");
-console.log(user1);
+// console.log(user1);
 var User2 = /** @class */ (function () {
     function User2(name, email, userId) {
         this.name = name;
@@ -22,4 +22,46 @@ var User2 = /** @class */ (function () {
     return User2;
 }());
 var user2 = new User2("Lakshman", "laksh@google.com", "123");
-console.log(user2);
+// console.log(user2);
+// getter and setter
+var newUser = /** @class */ (function () {
+    function newUser(name, email) {
+        this.name = name;
+        this.email = email;
+        this._courseCount = 1;
+        this.city = "Delhi";
+    }
+    newUser.prototype.deleteToken = function () {
+        console.log("Token deleted");
+    };
+    Object.defineProperty(newUser.prototype, "getAppleEmailId", {
+        get: function () {
+            return "apple".concat(this.email);
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(newUser.prototype, "getCourseCount", {
+        get: function () {
+            return this._courseCount;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(newUser.prototype, "setCourseCount", {
+        set: function (courseCount) {
+            if (courseCount <= 1) {
+                throw new Error("Course count should be more than 1");
+            }
+            this._courseCount = courseCount;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return newUser;
+}());
+var laksh = new newUser("Lakshman", "laksh@.com");
+laksh.setCourseCount = 4;
+console.log(laksh.getCourseCount); // 4
+// laksh.deleteToken(); //! error --> private method can't be accessed
+console.log(laksh.getAppleEmailId);
--- class.ts
+++ class.ts
@@ -9,7 +9,7 @@ class User1 {
 }
 
 const user1 = new User1("Lakshman", "laksh@google.com");
-console.log(user1);
+// console.log(user1);
 
 class User2 {
   readonly _id: string = "12345";
@@ -21,6 +21,42 @@ class User2 {
 }
 
 const user2 = new User2("Lakshman", "laksh@google.com", "123");
-console.log(user2);
+// console.log(user2);
+
+// getter and setter
+
+class newUser {
+  private _courseCount = 1;
+  readonly city: string = "Delhi";
+  constructor(
+    public name: string,
+    public email: string
+  ) {}
+
+  private deleteToken() {
+    console.log("Token deleted");
+  }
+
+  get getAppleEmailId(): string {
+    return `apple${this.email}`
+  }
+
+  get getCourseCount(): number {
+    return this._courseCount;
+  }
+
+  set setCourseCount(courseCount: number) { // cannot use any one of the return type
+    if (courseCount <= 1) {
+      throw new Error("Course count should be more than 1");
+    }
+    this._courseCount = courseCount;
+  }
+}
+
+const laksh = new newUser("Lakshman", "laksh@.com");
+laksh.setCourseCount = 4;
+console.log(laksh.getCourseCount); // 4
+// laksh.deleteToken(); //! error --> private method can't be accessed
+console.log(laksh.getAppleEmailId); // applelaksh@.com
 
 export {};
