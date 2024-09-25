25fd58bf518f888ef3ca3cd909e17dcce9a00933
Lakshman kumar <lakshmankumar2603@gmail.com>
2024-09-25 19:29:01 +0530

    generics class

--- generics-more.ts
+++ generics-more.ts
@@ -23,3 +23,29 @@ const result2 = getMoreSearchproducts<string>([
   "Watermelon",
 ]);
 console.log(result2); // Watermelon
+
+interface Database {
+  connection: string;
+  username: string;
+  password: string;
+}
+function anotherFunction<T, U extends Database>(arg1: T, arg2: U): Object {
+  return {
+    arg1,
+    arg2,
+  };
+}
+
+const result3 = anotherFunction<number, Database>(23, {
+  connection: "MongoDB",
+  username: "root",
+  password: "pass123",
+});
+console.log(result3);
+
+class sellable<T> {
+  public cart: T[] = [];
+  addToCart(product: T) {
+    this.cart.push(product);
+  }
+}
